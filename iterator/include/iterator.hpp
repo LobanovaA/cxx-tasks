b2faea67bec0a0dc22ce8117079f120df35c58bd
Anna Lobanova <a89856357783@gmail.com>
2018-12-02 18:27:19 +0300

    Лобораторная работа iterator. Лобанова Анна МП40

--- iterator/include/iterator.hpp
+++ iterator/include/iterator.hpp
@@ -16,9 +16,49 @@
 
 //{ image iterator
 template<class Iterator>
-class image_iterator: public boost::iterator_adaptor<...>
+class image_iterator: public boost::iterator_adaptor<image_iterator<Iterator>, Iterator>
 {
+public:
+    image_iterator(Iterator it, size_t width, size_t stride) : image_iterator::iterator_adaptor_(it), width_(width), stride_(stride) { }
+private:
+    friend class boost::iterator_core_access;
 
+    size_t width_;
+    size_t stride_;
+
+    size_t index = 0;
+
+    void increment()
+    {
+        advance(1);
+    }
+
+    void decrement()
+    {
+        advance(-1);
+    }
+
+    void advance(ptrdiff_t n)
+    {
+        int spacing = n;
+        int trash_count = stride_ - width_;
+        int offset = spacing > 0 ? (index % (int)stride_) : -(width_- index % (int)stride_);
+        int stride_count = (offset + spacing) / (int)width_;
+        spacing += stride_count * trash_count;
+
+        this->base_reference() += spacing;
+        index += spacing;
+    }
+
+    ptrdiff_t distance_to( image_iterator const& dst ) const
+    {
+        int delta = dst.base() - this->base();
+        int offset = delta > 0 ? this->index : dst.index % (int)stride_;
+
+        int pixel_count = abs(delta) + offset;
+
+        return (pixel_count / (int)stride_ * (int)width_ + pixel_count % (int)stride_ - offset)*(abs(delta)/delta);
+    }
 };
 //}
 
